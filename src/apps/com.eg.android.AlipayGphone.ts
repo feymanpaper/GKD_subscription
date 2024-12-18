fd7f0856703c09b89c8db6d389ba0b8f9522d365
feymanpaper <94893978+feymanpaper@users.noreply.github.com>
2024-12-18 18:34:01 +0800

    Update com.eg.android.AlipayGphone.ts

--- src/apps/com.eg.android.AlipayGphone.ts
+++ src/apps/com.eg.android.AlipayGphone.ts
@@ -1,24 +1,26 @@
 import { defineGkdApp } from '@gkd-kit/define';
 
 export default defineGkdApp({
-  "id": "com.eg.android.AlipayGphone",
-  "name": "支付宝",
-  "groups": [
+  id: 'com.eg.android.AlipayGphone',
+  name: '支付宝',
+  groups: [
     {
-      "key": 0,
-      "name": "in-app|3xWwJ0Krv2MQHPIIHzAzw7dowTPST50WiitC_eQduus=.png",
-      "matchTime": 10000,
-      "actionMaximum": 1,
-      "resetMatch": "app",
-      "priorityTime": 10000,
-      "rules": [
+      key: 0,
+      name: 'in-app|3xWwJ0Krv2MQHPIIHzAzw7dowTPST50WiitC_eQduus=.png',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      priorityTime: 10000,
+      rules: [
         {
-          "action": "clickCenter",
-          "activityIds": ".AlipayLogin",
-          "matches": "android.widget.FrameLayout > android.widget.FrameLayout > android.widget.FrameLayout > android.widget.LinearLayout > android.widget.LinearLayout > android.widget.Button[id=\"android:id/button1\"][text=\"DISAGREE\"][clickable=\"true\"]",
-          "snapshotUrls": ""
-        }
-      ]
-    }
-  ]
+          action: 'clickCenter',
+          activityIds: '.AlipayLogin',
+          matches: 'android.widget.FrameLayout > android.widget.FrameLayout > android.widget.FrameLayout > android.widget.LinearLayout > android.widget.LinearLayout > android.widget.Button[id="android:id/button1"][text="DISAGREE"][clickable="true"]',
+          snapshotUrls: '',
+        },
+
+      ],
+    },
+
+  ],
 });
