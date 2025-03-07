dc7e3699f88ce7824fb4df41f076050d1efb5490
feymanpaper <feymanpaper@gmail.com>
2025-03-07 12:10:44 +0800

    x

--- src/apps/com.alicloud.databox.ts
+++ src/apps/com.alicloud.databox.ts
@@ -23,23 +23,6 @@ export default defineGkdApp({
     },
     {
       key: 1,
-      name: 'in-app|47DEQpj8HBSa-_TImW-5JCeuQeRkm5NMpJWZG3hSuFU=.png',
-      matchTime: 10000,
-      actionMaximum: 1,
-      resetMatch: 'app',
-      priorityTime: 10000,
-      rules: [
-        {
-          action: 'clickCenter',
-          activityIds:
-            'com.alipay.mobile.nebulax.integration.mpaas.activity.NebulaActivity$Main',
-          matches: 'android.widget.FrameLayout',
-          snapshotUrls: 'https://i.gkd.li/i/13183946',
-        },
-      ],
-    },
-    {
-      key: 2,
       name: 'in-app|7Jwt86Bp38-ydIarqN125b15_uejKfBjdLw8O59Gxz8=.png',
       matchTime: 10000,
       actionMaximum: 1,
@@ -56,7 +39,7 @@ export default defineGkdApp({
       ],
     },
     {
-      key: 3,
+      key: 2,
       name: 'in-app|XXkgENA9RUKRqiLg5C2mQQZNQXCPqZzXILIVV-Oz5mY=.png',
       matchTime: 10000,
       actionMaximum: 1,
@@ -74,7 +57,7 @@ export default defineGkdApp({
       ],
     },
     {
-      key: 4,
+      key: 3,
       name: 'in-app|-DRKeOKY9cVgh6uN1_7l9T3JE-Tt2Q3QvWOpyB03vhk=.png',
       matchTime: 10000,
       actionMaximum: 1,
@@ -92,7 +75,7 @@ export default defineGkdApp({
       ],
     },
     {
-      key: 5,
+      key: 4,
       name: 'in-app|ADE7y_tCDcOAzwX4CM-DyZzo2mCGmB30h_uLPnErwhk=.png',
       matchTime: 10000,
       actionMaximum: 1,
@@ -109,7 +92,7 @@ export default defineGkdApp({
       ],
     },
     {
-      key: 6,
+      key: 5,
       name: 'in-app|cQCfIdAZesJ4MccFxLs1RURNIYnNEBfb98Z1XQoAlp8=.png',
       matchTime: 10000,
       actionMaximum: 1,
@@ -126,7 +109,7 @@ export default defineGkdApp({
       ],
     },
     {
-      key: 7,
+      key: 6,
       name: 'in-app|JPtkbpGn3k6gvPUPE08l-kjXwhnQY4FElOlrwvBrkAk=.png',
       matchTime: 10000,
       actionMaximum: 1,
@@ -143,7 +126,7 @@ export default defineGkdApp({
       ],
     },
     {
-      key: 8,
+      key: 7,
       name: 'in-app|yLuVO-y5frGyTkGVk-7ybXs759Sn2czCFGMyjpe-OFk=.png',
       matchTime: 10000,
       actionMaximum: 1,
@@ -160,7 +143,7 @@ export default defineGkdApp({
       ],
     },
     {
-      key: 9,
+      key: 8,
       name: 'in-app|PGnK9LEYOwdIfRDrtB3NRKFbjiolbi-u9RGhMOidWPE=.png',
       matchTime: 10000,
       actionMaximum: 1,
@@ -177,7 +160,7 @@ export default defineGkdApp({
       ],
     },
     {
-      key: 10,
+      key: 9,
       name: 'in-app|VPz0mZM0Hdu3RVhhyfBcLwHhBEOZGgb6om8BsxJjeH4=.png',
       matchTime: 10000,
       actionMaximum: 1,
@@ -194,7 +177,7 @@ export default defineGkdApp({
       ],
     },
     {
-      key: 11,
+      key: 10,
       name: 'in-app|08mVP-4_gECkFoDG6dKDMM3ei8XCcqLH9itfRscukuU=.png',
       matchTime: 10000,
       actionMaximum: 1,
@@ -211,7 +194,7 @@ export default defineGkdApp({
       ],
     },
     {
-      key: 12,
+      key: 11,
       name: 'in-app|47DEQpj8HBSa-_TImW-5JCeuQeRkm5NMpJWZG3hSuFU=.png',
       matchTime: 10000,
       actionMaximum: 1,
@@ -228,7 +211,7 @@ export default defineGkdApp({
       ],
     },
     {
-      key: 13,
+      key: 12,
       name: 'in-app|Ajpe93Ff-g16lXxdDCyNLwx41-zvoRkFl1RoRn9lHBI=.png',
       matchTime: 10000,
       actionMaximum: 1,
@@ -245,7 +228,7 @@ export default defineGkdApp({
       ],
     },
     {
-      key: 14,
+      key: 13,
       name: 'in-app|mRDiEHYjXOevA86y6AweN4FhQ2kLPw38e4HKeqxuhy4=.png',
       matchTime: 10000,
       actionMaximum: 1,
@@ -262,7 +245,7 @@ export default defineGkdApp({
       ],
     },
     {
-      key: 15,
+      key: 14,
       name: 'in-app|BMxd-jyoamPm8xjHghO3fWj_D9-2hK_WD8jPElw142Y=.png',
       matchTime: 10000,
       actionMaximum: 1,
