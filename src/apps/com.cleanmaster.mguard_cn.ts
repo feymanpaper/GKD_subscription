7c923b3a04bbd78e2802b57edf37e5d546c6fa5f
Flora1003Xu <1602456646@qq.com>
2025-03-13 08:18:23 +0800

    猎豹清理大师更新

--- src/apps/com.cleanmaster.mguard_cn.ts
+++ src/apps/com.cleanmaster.mguard_cn.ts
@@ -6,24 +6,6 @@ export default defineGkdApp({
   groups: [
     {
       key: 0,
-      name: 'in-app|7U8yBgSuyZ3Yb78817RuoEU71fpUTYAldmAVsQ0KckU=.png',
-      actionDelay: 3000,
-      actionMaximum: 3,
-      resetMatch: 'app',
-      priorityTime: 10000,
-      matchRoot: true,
-      rules: [
-        {
-          action: 'clickCenter',
-          activityIds: '',
-          matches:
-            'android.widget.FrameLayout > android.widget.FrameLayout > android.widget.FrameLayout[id="com.miui.systemAdSolution:id/view_main_container"] > android.widget.FrameLayout > android.widget.LinearLayout > android.widget.RelativeLayout[id="com.miui.systemAdSolution:id/view_ad_view_container"][clickable=true] > android.widget.FrameLayout[id="com.miui.systemAdSolution:id/view_splashAdSloganFlagView"] > android.widget.RelativeLayout > android.widget.LinearLayout[id="com.miui.systemAdSolution:id/view_ad_share_flag"] > android.widget.LinearLayout[id="com.miui.systemAdSolution:id/view_skip_button_nearby_top_right"][clickable=true]',
-          snapshotUrls: 'https://i.gkd.li/i/13183946',
-        },
-      ],
-    },
-    {
-      key: 1,
       name: 'in-app|DslH04uTiU541bYHWR-uESuhGb_3WS1TPE8uLM-D1Z4=.png',
       actionDelay: 3000,
       actionMaximum: 3,
@@ -41,7 +23,7 @@ export default defineGkdApp({
       ],
     },
     {
-      key: 2,
+      key: 1,
       name: 'in-app|fQtYwTowkO8ZnX3sC5xww4Akm0b0IyZcS7XLZx7uCOk=.png',
       actionDelay: 3000,
       actionMaximum: 3,
@@ -59,7 +41,7 @@ export default defineGkdApp({
       ],
     },
     {
-      key: 3,
+      key: 2,
       name: 'in-app|FxMPBXwo3Zm-sIQxtHkMcYHrU27_nOsJEO9ngrk6vtQ=.png',
       actionDelay: 3000,
       actionMaximum: 3,
@@ -77,7 +59,7 @@ export default defineGkdApp({
       ],
     },
     {
-      key: 4,
+      key: 3,
       name: 'system|GmbdVrGy-hLrkMmJtivGsTEl9xEN-RQU56nY5VDha4s=.png',
       actionDelay: 3000,
       actionMaximum: 3,
@@ -95,7 +77,7 @@ export default defineGkdApp({
       ],
     },
     {
-      key: 5,
+      key: 4,
       name: 'system|gX3BNwsAJcfr2Tk8kmWuNvMB6sy0T5cE4r-ZZjpsZEo=.png',
       actionDelay: 3000,
       actionMaximum: 3,
@@ -113,25 +95,7 @@ export default defineGkdApp({
       ],
     },
     {
-      key: 6,
-      name: 'in-app|h5qBdw-XiztnWucneWxfNEFKlGihS4FezpnU71U1RHw=.png',
-      actionDelay: 3000,
-      actionMaximum: 3,
-      resetMatch: 'app',
-      priorityTime: 10000,
-      matchRoot: true,
-      rules: [
-        {
-          action: 'clickCenter',
-          activityIds: '',
-          matches:
-            'android.widget.FrameLayout > android.widget.FrameLayout > android.widget.FrameLayout[id="android:id/content"] > android.widget.RelativeLayout > android.widget.Button[id="com.cleanmaster.mguard_cn:id/fw"][text="同意授权"][clickable=true]',
-          snapshotUrls: 'https://i.gkd.li/i/13183946',
-        },
-      ],
-    },
-    {
-      key: 7,
+      key: 5,
       name: 'in-app|I2IMUsOJKNVDkWiXtCk4DEouxQwfSN7U-BTaO393u9w=.png',
       actionDelay: 3000,
       actionMaximum: 3,
@@ -149,7 +113,7 @@ export default defineGkdApp({
       ],
     },
     {
-      key: 8,
+      key: 6,
       name: 'system|OG3-m0skPG4b-Uf96clX8FA_IoGkx3v3RTaBkKAyOdQ=.png',
       actionDelay: 3000,
       actionMaximum: 3,
@@ -167,43 +131,7 @@ export default defineGkdApp({
       ],
     },
     {
-      key: 9,
-      name: 'in-app|q4KZY47oDVd8-DmFoYgTf0sky401982SAD2k1aSxQto=.png',
-      actionDelay: 3000,
-      actionMaximum: 3,
-      resetMatch: 'app',
-      priorityTime: 10000,
-      matchRoot: true,
-      rules: [
-        {
-          action: 'clickCenter',
-          activityIds: '',
-          matches:
-            'android.widget.FrameLayout > android.widget.FrameLayout > android.widget.FrameLayout[id="android:id/content"] > android.widget.RelativeLayout > android.widget.Button[id="com.cleanmaster.mguard_cn:id/fw"][text="同意授权"][clickable=true]',
-          snapshotUrls: 'https://i.gkd.li/i/13183946',
-        },
-      ],
-    },
-    {
-      key: 10,
-      name: 'in-app|QJGEJ1Yl5QUYcxz_2zEXIuXhDXCu0dC2FB1AH1czMhw=.png',
-      actionDelay: 3000,
-      actionMaximum: 3,
-      resetMatch: 'app',
-      priorityTime: 10000,
-      matchRoot: true,
-      rules: [
-        {
-          action: 'clickCenter',
-          activityIds: '',
-          matches:
-            'android.widget.FrameLayout > android.widget.FrameLayout > android.widget.FrameLayout[id="android:id/content"] > android.widget.RelativeLayout > android.widget.Button[id="com.cleanmaster.mguard_cn:id/fw"][text="同意授权"][clickable=true]',
-          snapshotUrls: 'https://i.gkd.li/i/13183946',
-        },
-      ],
-    },
-    {
-      key: 11,
+      key: 7,
       name: 'in-app|qnKu1AoeDwk95888A25AyeXGA4M_SMvEcaNRFNkVIyc=.png',
       actionDelay: 3000,
       actionMaximum: 3,
@@ -221,7 +149,7 @@ export default defineGkdApp({
       ],
     },
     {
-      key: 12,
+      key: 8,
       name: 'in-app|S8WpCImWUOdB0jqr2VS1zlNEOU41uDvVgC2M6EPxrLs=.png',
       actionDelay: 3000,
       actionMaximum: 3,
@@ -239,7 +167,7 @@ export default defineGkdApp({
       ],
     },
     {
-      key: 13,
+      key: 9,
       name: 'system|wmA_kM5WskpOPMfVBOm-33VsbrVTNJg6lHNFCrtoXn0=.png',
       actionDelay: 3000,
       actionMaximum: 3,
@@ -257,25 +185,7 @@ export default defineGkdApp({
       ],
     },
     {
-      key: 14,
-      name: 'in-app|xGcbdtK92MgAK1qdWexOpXV_5tEqLlJd8WsYnjZW8a4=.png',
-      actionDelay: 3000,
-      actionMaximum: 3,
-      resetMatch: 'app',
-      priorityTime: 10000,
-      matchRoot: true,
-      rules: [
-        {
-          action: 'clickCenter',
-          activityIds: '',
-          matches:
-            'android.widget.FrameLayout > android.widget.FrameLayout > android.widget.FrameLayout[id="android:id/content"] > android.widget.RelativeLayout > android.widget.ImageView[id="com.cleanmaster.mguard_cn:id/f4"][clickable=true]',
-          snapshotUrls: 'https://i.gkd.li/i/13183946',
-        },
-      ],
-    },
-    {
-      key: 15,
+      key: 10,
       name: 'in-app|YZQvbxfXYpGEb_8Uj-dArgHjIenvCuznRkMT0N45vZI=.png',
       actionDelay: 3000,
       actionMaximum: 3,
@@ -288,24 +198,6 @@ export default defineGkdApp({
           activityIds: '',
           matches:
             'android.widget.FrameLayout > android.widget.FrameLayout > android.widget.FrameLayout[id="android:id/content"] > android.widget.LinearLayout > android.widget.LinearLayout[id="com.cm.plugin.internal:id/jp"] > android.widget.RelativeLayout[id="com.cm.plugin.internal:id/fo"][clickable=true]',
-          snapshotUrls: 'https://i.gkd.li/i/13183946',
-        },
-      ],
-    },
-    {
-      key: 16,
-      name: 'in-app|zDjo8pJPUx3mTjhkoj7ZEsVfZsbUUyMNQPPyY_frGM8=.png',
-      actionDelay: 3000,
-      actionMaximum: 3,
-      resetMatch: 'app',
-      priorityTime: 10000,
-      matchRoot: true,
-      rules: [
-        {
-          action: 'clickCenter',
-          activityIds: '',
-          matches:
-            'android.widget.FrameLayout > android.widget.LinearLayout > android.widget.FrameLayout[id="android:id/content"] > android.widget.FrameLayout[id="com.cleanmaster.mguard_cn:id/a99"] > android.widget.FrameLayout[id="com.cleanmaster.mguard_cn:id/zh"] > android.view.ViewGroup[id="com.cleanmaster.mguard_cn:id/an3"] > android.widget.RelativeLayout > android.widget.ListView[id="com.cleanmaster.mguard_cn:id/zf"] > android.widget.LinearLayout > android.widget.RelativeLayout[id="com.cleanmaster.mguard_cn:id/a99"] > android.view.ViewGroup[id="com.cleanmaster.mguard_cn:id/a52"] > android.widget.RelativeLayout > android.widget.RelativeLayout > android.view.ViewGroup[id="com.cleanmaster.mguard_cn:id/rw"] > android.widget.LinearLayout > android.widget.FrameLayout[id="com.cleanmaster.mguard_cn:id/x0"] > android.widget.RelativeLayout[id="com.cleanmaster.mguard_cn:id/ah5"][clickable=true]',
           snapshotUrls: 'https://i.gkd.li/i/13183946',
         },
       ],
